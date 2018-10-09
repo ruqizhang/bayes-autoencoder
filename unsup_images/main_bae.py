2135efaa2a3e26612ee7b93a9d4fe7fe14e23fc9
wjmaddox <wm326@cornell.edu>
2018-10-09 10:47:48 -0400

    first commit of bae rewrite

---
+++ unsup_images/main_bae.py
@@ -0,0 +1,257 @@
+# coding: utf-8
+import argparse
+import time
+import math
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import models
+import torch.optim as optim
+import numpy as np
+from torch.autograd.gradcheck import zero_gradients
+import sys, os
+sys.path.append('..')
+import data, utils
+parser = argparse.ArgumentParser(description='PyTorch PTB RNN/LSTM Language Model')
+parser.add_argument('--dataset', type = str, default = 'MNIST')
+parser.add_argument('--data_path', type=str, default='./datasets/ptb',
+                    help='location of the MNIST dataset ')
+parser.add_argument('--model', type=str, default='BAE')
+parser.add_argument('--nhid', type=int, default=200,
+                    help='number of hidden units per layer')
+parser.add_argument('--zdim', type=int, default=20,
+                    help='number of hidden units per layer')
+parser.add_argument('--lr', type=float, default=1,
+                    help='initial learning rate')
+parser.add_argument('--clip', type=float, default=0.25,
+                    help='gradient clipping')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='upper epoch limit')
+parser.add_argument('--use_test', dest='use_test', action='store_true', help='use test dataset instead of validation (default: False)')
+parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                    help='batch size')
+parser.add_argument('--seed', type=int, default=1,
+                    help='random seed')
+parser.add_argument('--no-cuda', action='store_true',
+                    help='do not use CUDA')
+parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                    help='report interval')
+parser.add_argument('--save-epochs', type=int, default=25, metavar='N',
+                    help='how often to save the model')
+parser.add_argument('--dir', type=str, required = True)
+#parser.add_argument('--device_id',type = int, help = 'device id to use')
+args = parser.parse_args()
+#device_id = args.device_id
+
+###############################################################################
+# Load data and prepare model
+###############################################################################
+#set cuda if available, while also setting seed
+torch.manual_seed(args.seed)
+args.device = None
+if torch.cuda.is_available() and not args.no_cuda:
+    torch.cuda.manual_seed(args.seed)
+    torch.backends.cudnn.benchmark = True
+    args.device = torch.device('cuda')
+else:
+    args.device = torch.device('cpu')
+
+#define the model
+print('Using model %s' % args.model)
+model_cfg = getattr(models, args.model)
+
+print('Preparing model')
+print(*model_cfg.args)
+print('using ', args.zdim, ' latent space')
+model = model_cfg.base(*model_cfg.args, noise_dim=args.zdim, zdim=args.zdim, **model_cfg.kwargs)
+model.cuda()
+
+#prepare the dataset
+print('Loading dataset %s from %s' % (args.dataset, args.data_path))
+loaders = data.loaders(
+    args.dataset,
+    args.data_path,
+    args.batch_size,
+    4,
+    model_cfg.transform_train,
+    model_cfg.transform_test,
+    use_validation=not args.use_test,
+    unsup=True
+)
+datasize = len(loaders['train'].dataset)
+
+#make directory
+print('Preparing directory %s' % args.dir)
+os.makedirs(args.dir, exist_ok=True)
+os.makedirs(args.dir+'/results/', exist_ok=True)
+with open(os.path.join(args.dir, 'command.sh'), 'w') as f:
+    f.write('python ' + ' '.join(sys.argv))
+    f.write('\n')
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
+
+def loss_function(recon_batch, x):
+    recon_batch = recon_batch.view(-1,784)
+    #BCE = F.cross_entropy(recon_batch, x)
+    l_dist =  torch.distributions.Bernoulli(probs = recon_batch)
+    likelihood = -l_dist.log_prob(x).sum()/x.size(0)
+    return likelihood
+
+def en_loss(z_recon,z):
+    z = Variable(z.data,requires_grad = False)
+    loss = F.mse_loss(z_recon,z)
+    return loss
+
+def z_prior_loss(z):
+    prior_loss = 0.5*torch.sum(z*z)
+    return prior_loss
+
+def z_noise_loss(z):
+
+    learning_rate = lr
+
+    noise_std = np.sqrt(2*learning_rate*alpha)
+    noise_std = torch.from_numpy(np.array([noise_std])).float().cuda()
+    noise_std = noise_std[0]
+    random_var = torch.ones_like(z).normal_() * noise_std
+    noise_loss = torch.sum(z * random_var)
+    #means = torch.zeros(z.size()).cuda()
+    #noise_loss = torch.sum(z * Variable(torch.normal(means, std = noise_std).cuda(),
+    #                       requires_grad = False))
+    return noise_loss
+
+def evaluate(data_source, epoch, dir):
+    # Turn on evaluation mode which disables dropout.
+    model.eval()
+    total_loss = 0
+    total_kld = 0
+    #ntokens = len(corpus.dictionary)
+    count=0
+    # hidden = model.init_hidden(eval_batch_size)
+    for i, (data,_) in enumerate(data_source):
+        data = data.view(-1, 784).cuda()
+        if i==0 and epoch%args.save_epochs is 0:
+            model.reconstruct_samples(data, epoch = epoch, dir = dir)
+        #data, targets = get_batch(data_source, i)
+        #model.decoder.bsz = eval_batch_size
+        recon_batch,z,xi = model(data)
+        BCE = loss_function(recon_batch, data)
+
+        loss = BCE + (z_prior_loss(z).sum())/10000.
+
+        total_loss += loss.data[0]
+        count+=1
+    avg = total_loss / count
+    #print(' ppl_avg :%g avg_loss:%g ' % (math.exp(avg),avg ))
+    return avg
+
+def z_opt(z_sample):
+    opt = optim.SGD([z_sample], lr=lr, momentum = 1-alpha)
+    return opt
+
+def train():
+    # Turn on training mode which enables dropout.
+    model.train()
+    total_loss = 0
+    start_time = time.time()
+    #ntokens = len(corpus.dictionary)
+    #model.decoder.bsz = args.batch_size
+    # hidden = model.init_hidden(args.batch_size)
+    #for batch, i in enumerate(range(0, train_data.size(0) - 1, args.bptt)):
+    for i, (data, _) in enumerate(loaders['train']):
+        data = data.view(-1, 784).cuda()
+        #data, targets = get_batch(train_data, i)
+
+        # Starting each batch, we detach the hidden state from how it was previously produced.
+        # If we didn't, the model would try backpropagating all the way to start of the dataset.
+        # hidden = repackage_hidden(hidden)
+        for j in range(J):
+            if j == 0:
+                model.zero_grad()
+                recon_batch,z,xi = model(data)
+                z_sample = Variable(z.data,requires_grad = True)
+                z_optimizer = z_opt(z_sample)
+                z_optimizer.zero_grad()
+            else:
+                recon_batch = model.decoder(z_sample)
+
+            BCE = loss_function(recon_batch, data)
+
+            prior_loss = model.prior_loss(prior_std)
+            noise_loss = model.noise_loss(lr,alpha)
+            prior_loss /= datasize
+            noise_loss /= datasize
+            prior_loss_z = z_prior_loss(z_sample)
+            noise_loss_z = z_noise_loss(z_sample)
+            prior_loss_z /= datasize
+            noise_loss_z /= datasize
+            loss = BCE + prior_loss +noise_loss + prior_loss_z + noise_loss_z
+
+            if j>burnin:
+                loss_en = en_loss(z_sample,z)
+                loss += loss_en
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm(model.parameters(), args.clip)
+            optimizer.step()
+            z_optimizer.step()
+
+        total_loss += BCE.data
+
+        if i % args.log_interval == 0 and i > 0:
+            cur_loss = total_loss.item() / args.log_interval
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:5.5f} | ms/batch {:5.2f} | '
+                    'loss {:5.2f}'.format(
+                epoch, i, int(datasize/args.batch_size), lr,
+                elapsed * 1000 / args.log_interval, cur_loss))
+            total_loss = 0
+            start_time = time.time()
+
+# Loop over epochs.
+lr = args.lr
+alpha = 0.1
+bit = 0
+prior_std = 1
+epoch_cut = 0
+burnin = 0
+J = 2
+best_val_loss = None
+optimizer = optim.SGD(model.parameters(), lr=lr,momentum = 1-alpha)
+# At any point you can hit Ctrl + C to break out of training early.
+try:
+    for epoch in range(1, args.epochs+1):
+        epoch_start_time = time.time()
+        train()
+        #val_loss = evaluate(val_data)
+        #print('-' * 89)
+        #print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+        #        'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
+        #                                   val_loss, math.exp(val_loss)))
+        print('-' * 89)
+        test_loss = evaluate(loaders['test'], epoch = epoch, dir = args.dir+ '/results')
+        if epoch%args.save_epochs is 0:
+            print('saving model')
+            #torch.save(model.state_dict().clone().cpu(), 
+            utils.save_model(epoch, model, optimizer, args.dir)
+            model.generate_samples(dir = args.dir +'/results', epoch=epoch)
+            
+        print('-' * 89)
+        print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
+                .format(epoch, (time.time() - epoch_start_time),
+                                           test_loss))
+        print('-' * 89)
+        # Save the model if the validation loss is the best we've seen so far.
+        #if not best_val_loss or val_loss < best_val_loss:
+        #    print('New Best!')
+            # torch.save(model.state_dict(),'model.pt')
+        #    best_val_loss = val_loss
+except KeyboardInterrupt:
+    print('-' * 89)
+    print('Exiting from training early')
--- unsup_images/models/bae.py
+++ unsup_images/models/bae.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 
 from torchvision import transforms
+from torchvision.utils import save_image
+import numpy as np
+from torch.autograd import Variable
 
 from .mlp import *
 
@@ -16,10 +19,11 @@ class BayesAE(nn.Module):
         self.dim, self.zdim, self.hidden, self.noise_dim = dim, zdim, hidden, noise_dim
         self.encoder = MLP(dim+noise_dim, hidden, zdim, activation = activation)
         self.decoder = MLP(zdim, hidden, dim, activation = activation)
-        
+
     def encode(self, x):
-        x_new = torch.cat((x, torch.ones(x.size(0), self.noise_dim, device=x.device, dtype=x.dtype).normal_()),dim=1)
-        return self.encoder(x_new)
+        noise = torch.ones(x.size(0), self.noise_dim, device=x.device, dtype=x.dtype).normal_()
+        x_new = torch.cat((x.view(-1, self.dim), noise),dim=1)
+        return self.encoder(x_new), noise
     
     def decode(self, z):
         return nn.Sigmoid()(self.decoder(z))
@@ -28,8 +32,52 @@ class BayesAE(nn.Module):
     def forward(self, x):
         x = x.view(-1,self.dim)
 
-        mu, logvar = self.encode(x)
-        return mu, logvar  
+        z, noise = self.encode(x)
+        reconstructed_batch = self.decode(z)
+        return reconstructed_batch, z, noise
+    
+    def prior_loss(self,prior_std):
+        prior_loss = 0.0
+        for var in self.parameters():
+            nn = torch.div(var, prior_std)
+            prior_loss += torch.sum(nn*nn)
+        #print('prior_loss',prior_loss)#1e-3
+        return 0.5*prior_loss
+
+    def noise_loss(self,lr,alpha):
+        noise_loss = 0.0
+        # learning_rate = base_lr * np.exp(-lr_decay *min(1.0, (train_iter*args.batch_size)/float(datasize)))
+        learning_rate = lr
+        noise_std = np.sqrt(2*learning_rate*alpha)
+        noise_std = torch.from_numpy(np.array([noise_std])).float().cuda()
+        noise_std = noise_std[0]
+        for var in self.parameters():
+            #means = torch.zeros(var.size()).cuda()
+            means = torch.zeros_like(var)
+            random_var = torch.ones_like(var).normal_() * noise_std
+            #noise_loss += torch.sum(var * Variable(torch.normal(means, std = noise_std).cuda(),
+            #                   requires_grad = False))
+            noise_loss += torch.sum(var * random_var)
+        #print('noise_loss',noise_loss)#1e-8
+        return noise_loss 
+    
+    def reconstruct_samples(self, data, dir, epoch, **kwargs):
+        z_gen, _ = self.encode(data.view(-1, self.dim))
+    
+        recon_batch = self.decode(z_gen)
+        n = min(data.size(0), 8)
+        comparison = torch.cat([data.view(-1, 1, 28, 28)[:n],
+                    recon_batch.view(data.size(0), 1, 28, 28)[:n]])
+        save_image(comparison.data.cpu(),
+             dir + '/reconstruction_' + str(epoch) + '.png', nrow=n)
+    
+    def generate_samples(self, dir, epoch):
+        #sample = z_prior.sample((64,))
+        sample = torch.randn(64, self.zdim).cuda()
+        
+        sample = self.decode(sample).cpu()
+        save_image(sample.data.view(64, 1, 28, 28),
+                dir + '/sample_' + str(epoch) + '.png') 
 
 class BayesAE_SS(nn.Module):
     def __init__(self, dim = 784, noise_dim = 10, zdim = 50, hidden = 500, activation = nn.ReLU):
